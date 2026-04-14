10c069d1b393423202275a07a99f842a094cfd33
Dyuti Mengji <73112092+DyutiM25@users.noreply.github.com>
2026-04-14 15:24:54 -0700

    Revert "Dyuti main"

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3001/api"
+      : "/api",
   withCredentials: true,
 });
 
