428c939e5de12bdd0d58ac7456ad8418d415e24a
Mengji Dhanush <mengji.dhanush16@gmail.com>
2026-04-14 09:02:32 +0530

    Change CORS origin to Netlify URL

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://dr32teeth.netlify.app",
     credentials: true,
   })
 );
